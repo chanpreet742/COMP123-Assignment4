50d491d0fd05fb6267d12a1494caf4383609ff06
300932732 <300932732@PR-A315-30>
2017-07-28 15:49:01 -0400

    Implemented the IHasMoons interface

--- COMP123-Assignment4/IHasMoons.cs
+++ COMP123-Assignment4/IHasMoons.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
  * Name: Chanpreet Mudhar
  * Student Number - 300932732
  * Date: July 28, 2017
- * Description: This is a class called 'GiantPlanet' which is a subclass of the 'Planet' abstract class.
+ * Description: This is the IHasMoons interface.
  * Version: 0.1 - 
  */
 namespace COMP123_Assignment4
 {
+    /// <summary>
+    /// This is the IHasMoons Interface.
+    /// This includes a method header HasMoons 
+    /// that returns a boolean value.
+    /// </summary>
     interface IHasMoons
     {
+        bool HasMoons();
     }
 }
