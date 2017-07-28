36a5fb7f35934a66f8bb24b056bf1382a1dba6e0
300932732 <300932732@PR-A315-30>
2017-07-28 15:32:56 -0400

    Implemented HasMoons() and HasRings() method

--- COMP123-Assignment4/GiantPlanet.cs
+++ COMP123-Assignment4/GiantPlanet.cs
@@ -8,18 +8,54 @@ using System.Threading.Tasks;
  * Student Number - 300932732
  * Date: July 28, 2017
  * Description: This is a class called 'GiantPlanet' which is a subclass of the 'Planet' abstract class.
- * Version: 0.1 - 
+ * Version: 0.2- Implemented HasMoons() and HasRings() method
  */
 namespace COMP123_Assignment4
 {
     // This is the GiantPlanet class
 
-    class GiantPlanet
+    class GiantPlanet : Planet,IHasMoons,IHasRings
     {
-        // PRIVATE FIELDS (Instance Variables)
+        // PRIVATE FIELDS (Instance Variables) ~~~~~~~~~~
         string _type;
 
-        // PUBLIC PROPERTIES 
+        // CONSTRUCTORS ~~~~~~~~~~~~
+        /// <summary>
+        /// This is the public constructor of the GiantPlanet class
+        /// It takes four arguments-
+        /// -name(string)
+        /// -diameter(double)
+        /// -mass(double)
+        /// -type(string)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
+        public GiantPlanet(String name,double diameter,double mass,string type)
+            :base(name,diameter,mass)
+        {
+            this._type = type;
+        }
+        public bool HasMoons()
+        {
+            if (MoonCount > 0)
+            
+                return true;
+
+                else
+
+                return false;
+            
+        }
+        public bool HasRings()
+        {
+            if (RingCount > 0)
+                return true;
+            else
+                return false;
+        }
+
 
     }
 }
